6537623dca97d74db9db9e6d64be565f180c2db4
zaksotm <zak@sotm.com>
2020-03-05 10:48:17 -0600

    Updates

---
+++ precache-manifest.fbcd95668d7fb4222ae922ff21b228d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8138ca17e8fd54c39551f3f91231c0be",
+    "url": "/testpublish/index.html"
+  },
+  {
+    "revision": "9e06b60460bf5b93de2b",
+    "url": "/testpublish/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b1379c831a8dd6a0cd6",
+    "url": "/testpublish/static/js/2.f42c2719.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/testpublish/static/js/2.f42c2719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e06b60460bf5b93de2b",
+    "url": "/testpublish/static/js/main.e077dbe1.chunk.js"
+  },
+  {
+    "revision": "14dfbde11c3af185bcad",
+    "url": "/testpublish/static/js/runtime-main.3ea811b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testpublish/static/media/logo.5d5d9eef.svg"
+  }
+]);
